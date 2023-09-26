88c92fea01e5fe72f1609e18478716fc3d6f16b5
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-26 17:19:05 +0200

    feat: :sparkles: Completed button excercise

--- ts-react/index.tsx
+++ ts-react/index.tsx
@@ -1,1 +1,17 @@
 import React from "react";
+
+interface buttonProps {
+  className: string;
+}
+export const Button = (props: buttonProps) => {
+  return <button>{props.className}</button>;
+};
+
+export const Component = () => {
+  return (
+    <div>
+      <p>Hello There</p>
+      <Button className="my-class"></Button>
+    </div>
+  );
+};
